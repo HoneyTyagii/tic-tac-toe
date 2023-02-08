c1d7fdc24cb26291abc8e6849179ba9410d71bcb
Honey Tyagi <honeyt290@gmail.com>
2023-02-08 23:46:09 +0530

    commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 80;
  
 //routes
 app.get("/", (req, res) => { 
-    res.render("index");       
+    res.render("index");               
 })
 //creating unique id by uuid and rediricting the route
 app.get("/uuid", (req, res) => {
